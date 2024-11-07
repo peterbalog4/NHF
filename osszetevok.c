645216a16e64a4a726261bfc9f37d36302a5783d
peterbalog4 <133996119+peterbalog4@users.noreply.github.com>
2024-11-07 09:24:41 +0100

    Add files via upload

--- osszetevok.c
+++ osszetevok.c
@@ -2,53 +2,14 @@
 #include <stdbool.h>
 #include <windows.h>
 #include <string.h>
+#include "uf.h"
 
 // Megjegyzések a kódhoz:
 // 1. Minden összetevő sztring 52 méretű. A felhasználónak 50 karakter lett "megígérve". A sorokat_szamol függvényem \n karaktereket számol így ez kell,hogy
 // minden sztring végén ott legyen. Így a lezáró nullával együtt minden összetevő sztring 50+1+1 méretű.
- int sorokat_szamol(char *fajl){
-    FILE *fp;
-    fp = fopen(fajl,"r");
-    char c;
-    int sorszamolo = 0;
-    while(fscanf(fp,"%c",&c) != EOF){
-        if(c == '\n'){
-            sorszamolo++;
-        }
-        else
-            continue;
-    }
-    fclose(fp);
-    return sorszamolo;
-}
+// 2. A segédfüggvények az uf.c fájlban vannak.
 
-//Később kellhet
-
-/*void osszetevot_listasit(char *fajl){
-    char osszetevok[];
-    *fp = fopen(fajl,"r");
-    int i=0;
-    do{
-        fscanf(fp,"%s",&osszetevok[i++]);
-    }
-    while()
-} */
-
-// Forrás : A sztringet_nagybetusit függvény a 6. heti előadásból lett átvéve.
-void sztringet_nagybetusit(char *sztring) {
-    for (int i = 0; sztring[i] != '\0'; ++i)
-        sztring[i] = toupper(sztring[i]);
-}
-int valaszt_tesztel(char *valasz){
-    if(strcmp(valasz,"IGEN") == 0 || strcmp(valasz,"I") == 0 )
-        return 1;
-    if(strcmp(valasz,"NEM") == 0 || strcmp(valasz,"N") == 0)
-        return 0;
-    else
-        return -1;
-
-
-}
+// NTS: Fájlkezelést javítani!!! || Fájlt az elején beolvassa, végén fájlba ír
 
 void uj_osszetevo(void){
     bool van_e;
@@ -67,8 +28,8 @@ void uj_osszetevo(void){
             //strcat(ujosszetevo, "\n");
             fprintf(fp,"%s\n",ujosszetevo);
         }else if(valaszt_tesztel(valasz) == 0) {
-            van_e = false;
             fclose(fp);
+            van_e = false;
         }
         else{
             printf("\nIsmeretlen valasztas kerlek az igen/nem szavakat vagy I/H betut adj meg\n");
@@ -76,7 +37,7 @@ void uj_osszetevo(void){
         }
     }while(van_e);
 }
-// NTS: Kell egy függvény ami felszabadítja a listát!!!
+
 char **osszetevo_lista(){
     FILE *fp;
     char **osszetevok;
@@ -117,30 +78,40 @@ void osszetevot_torol(){
     int valasztas;
     FILE *fp;
     fp = fopen("osszetevok.txt","w");
-    printf("Az összetevő sorszámának megadásával válaszd ki a törölni kívánt elemet: \n");
-    for(int i=0;i<meret;i++){
-        printf("%d. %s \n",i+1,lista[i]);
-    }
-    printf("Választás:");
-    scanf("%d",&valasztas);
-    if(valasztas>meret){
-        printf("Nem megfelelő számot adtál meg!");
-        printf("Választás:");
-        scanf("%d",&valasztas);
-    }
-    for(int i=0;i<meret;i++){
-        if (valasztas== i){
+    bool van_e = true;
+    char valasz[5];
+    while(van_e == true){
+        printf("Van még törlendő elem?");
+        scanf("%s",&valasz);
+        if(valaszt_tesztel(valasz) == 1){
             continue;
         }
-        else
-            fputs(lista[i],fp);
+        else if (valaszt_tesztel(valasz) == 0) {
+            van_e = false;
+        }
+        else{
+            printf("\nIsmeretlen valasztas kerlek az igen/nem szavakat vagy I/H betut adj meg\n");
+            van_e = true;
+        }
+        printf("Az összetevő sorszámának megadásával válaszd ki a törölni kívánt elemet: \n");
+        for(int i=0;i<meret;i++){
+            printf("%d. %s \n",i+1,lista[i]);
+        }
+        printf("Választás:");
+        scanf("%d",&valasztas);
+        if(valasztas>meret){
+            printf("Nem megfelelő számot adtál meg!");
+            printf("Választás:");
+            scanf("%d",&valasztas);
+        }
+        for(int i=0;i<meret;i++){
+            if (valasztas== i){
+                continue;
+            }
+            else
+                fputs(lista[i],fp);
+        }
     }
-    fclose(fp);
-    osszetevot_felszabadit(lista,meret);
-}
-int main() {
-
-    system("chcp 65001 >nul");
-    uj_osszetevo();
-    osszetevot_torol();
+        fclose(fp);
+        osszetevot_felszabadit(lista,meret);
 }
---
+++ osszetevok.h
@@ -0,0 +1,8 @@
+#ifndef OSSZETEVOK_H_INCLUDED
+#define OSSZETEVOK_H_INCLUDED
+
+void uj_osszetevo(void);
+char **osszetevo_lista();
+void osszetevot_felszabadit(char **osszetevok, int meret);
+void osszetevot_torol();
+#endif // OSSZETEVOK_H_INCLUDED
