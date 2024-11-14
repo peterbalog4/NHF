aec09dfe843f31c908b7122a771d59923f5d3d86
Peter Balog <133996119+peterbalog4@users.noreply.github.com>
2024-11-14 11:58:31 +0100

    Add files via upload

--- teszt.c
+++ teszt.c
@@ -7,7 +7,10 @@
 typedef struct Recept{
     char nev[51];
     char **o_lista;
+    int o_meret;
+    int **ml;
     char **el_lista;
+    int el_meret;
     struct Recept *kov;
 } Recept;
 
@@ -23,16 +26,29 @@ Recept *uj_recept(Recept *eleje){
     printf("Hány összetevő legyen?");
     scanf("%d", &o_meret);
 
-    char **o_lista = malloc(o_meret*sizeof(char *));
-    if (o_lista == NULL){
+    uj->o_lista = malloc(o_meret*sizeof(char *));
+    if (uj->o_lista == NULL){
         printf("Memóriafoglalási hiba!");
         exit(1);
     }
     for(int i=0;i<o_meret;i++){
-        o_lista[i] = malloc(52 * sizeof(char));
-        if (o_lista[i] == NULL){
+        uj->o_lista[i] = malloc(52 * sizeof(char));
+        if (uj->o_lista[i] == NULL){
             printf("Memóriafoglalási hiba!");
             exit(2);
+        }
+    }
+
+    uj->ml = malloc(o_meret*sizeof(int *));
+    if (uj->ml == NULL){
+        printf("Memóriafoglalási hiba!");
+        exit(7);
+    }
+    for(int i=0;i<o_meret;i++){
+        uj->ml[i] = malloc(sizeof(int));
+        if (uj->ml[i] == NULL){
+            printf("Memóriafoglalási hiba!");
+            exit(8);
         }
     }
 
@@ -41,7 +57,9 @@ Recept *uj_recept(Recept *eleje){
         for(int i=0;i < o_meret;i++){
             printf("Választás:");
             scanf("%d",&valasz);
-            o_lista[i] = lista[valasz-1];
+            strcpy(uj->o_lista[i],lista[valasz-1]);
+            printf("Hány ml-t?");
+            scanf("%d",uj->ml[i]);
         }
     }
     else{
@@ -53,14 +71,14 @@ Recept *uj_recept(Recept *eleje){
     printf("Hány lépés legyen az elkészítési leírás?");
     scanf("%d",&el_meret);
 
-    char **el_lista = malloc(el_meret*sizeof(char *));
-    if (el_lista == NULL){
+    uj->el_lista = malloc(el_meret*sizeof(char *));
+    if (uj->el_lista == NULL){
         printf("Memóriafoglalási hiba!");
         exit(4);
     }
     for(int i=0;i<el_meret;i++){
-        el_lista[i] = malloc(201 * sizeof(char));
-        if (el_lista[i] == NULL){
+        uj->el_lista[i] = malloc(201 * sizeof(char));
+        if (uj->el_lista[i] == NULL){
             printf("Memóriafoglalási hiba!");
             exit(5);
         }
@@ -69,32 +87,49 @@ Recept *uj_recept(Recept *eleje){
     if(el_meret >= 0){
         for(int i=0;i < el_meret;i++){
             printf("%d. lépés:",i+1);
-            scanf("%200s", el_lista[i]);
+            scanf("%200s", uj->el_lista[i]);
         }
     }
     else{
         exit(6);
     }
+    uj->o_meret = o_meret;
+    uj->el_meret = el_meret;
 
-    uj->o_lista = o_lista;
-    uj->el_lista = el_lista;
 
     uj->kov = eleje;
     eleje = uj;
 
     osszetevot_felszabadit(lista,meret);
-    osszetevot_felszabadit(el_lista,el_meret);
-    osszetevot_felszabadit(o_lista,o_meret);
+    //osszetevot_felszabadit(el_lista,el_meret);
+    //osszetevot_felszabadit(o_lista,o_meret);
 
     return uj;
 }
-
+void receptet_kiir(Recept *recept){
+    if (recept != NULL) {
+        printf("Név: %s\n", recept->nev);
+        printf("Összetevők: \n");
+        for(int i=0;i<recept->o_meret;i++){
+            printf("%s %d ml \n",recept->o_lista[i],recept->ml[i]);
+        }
+        printf("Elkészítési leírás: \n");
+        for(int i=0;i<recept->el_meret;i++){
+            printf("%s \n",recept->el_lista[i]);
+        }
+    }
+}
 int main(){
     system("chcp 65001 >nul");
     Recept *eleje = NULL;
     Recept *uj = uj_recept(eleje);
 
-    if (uj != NULL) {
-        printf("Recept hozzáadva: %s\n", uj->nev);
+    receptet_kiir(uj);
+    osszetevot_felszabadit(uj->el_lista,uj->el_meret);
+    osszetevot_felszabadit(uj->o_lista,uj->o_meret);
+    for(int i=0;i<uj->o_meret;i++){
+        free(uj->ml[i]);
     }
+    free(uj->ml);
+    free(uj);
 }
