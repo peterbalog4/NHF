12d14bf7c90cdde7085d755531f51476418ad170
Peter Balog <133996119+peterbalog4@users.noreply.github.com>
2024-11-14 09:28:31 +0100

    Update recept.h

--- recept.h
+++ recept.h
@@ -1,6 +1,7 @@
 #ifndef RECEPT_H_INCLUDED
 #define RECEPT_H_INCLUDED
 
+typedef struct Recept{char nev[51];char **o_lista;char **el_lista;struct Recept *kov;} Recept;
 Recept *uj_recept(Recept *eleje)
 
 #endif // RECEPT_H_INCLUDED
