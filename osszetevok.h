9a0af05220d6af1d1b22ac14b068cd2fea8f9f94
Peter Balog <133996119+peterbalog4@users.noreply.github.com>
2024-11-14 08:45:45 +0100

    Add files via upload

--- osszetevok.h
+++ osszetevok.h
@@ -1,7 +1,7 @@
 #ifndef OSSZETEVOK_H_INCLUDED
 #define OSSZETEVOK_H_INCLUDED
 
-void uj_osszetevo(void);
+void uj_osszetevo(char **lista,int meret);
 char **osszetevo_lista();
 void osszetevot_felszabadit(char **osszetevok, int meret);
 void osszetevot_torol();
