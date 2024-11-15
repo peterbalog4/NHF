0f2d2ff942f6caa61c69529cce36215ec7914ee0
Peter Balog <133996119+peterbalog4@users.noreply.github.com>
2024-11-15 12:40:28 +0100

    Update recept.h

--- recept.h
+++ recept.h
@@ -3,5 +3,6 @@
 
 typedef struct Recept{char nev[51];char **o_lista;int o_meret;int **ml;char **el_lista;int el_meret;struct Recept *kov;} Recept;
 void uj_recept(Recept **eleje);
+void receptet_kiir(Recept *recept);
 
 #endif // RECEPT_H_INCLUDED
